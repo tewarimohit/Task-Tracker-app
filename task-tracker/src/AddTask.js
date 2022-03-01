734f9c14295cc85a0ea36909da5e670113aac5be
Mohit Tewari <tewarimohit955@gmail.com>
2022-03-01 13:02:32 +0530

    Docs: AddTask.js

--- task-tracker/src/AddTask.js
+++ task-tracker/src/AddTask.js
@@ -1,18 +1,56 @@
-import Task from "./Task";
+import { useState } from "react";
 
-const Tasks = props => {
+const AddTask = prop => {
+	const [text, setText] = useState("");
+	const [day, setDay] = useState("");
+	const [remainder, setRemainder] = useState(false);
+
+	const taskIs = event => setText(event.target.value);
+	const dayIs = event => setDay(event.target.value);
+	const remainderIs = event => setRemainder(event.currentTarget.checked);
+	const onSubmit = event => {
+		event.preventDefault();
+
+		if (!text) {
+			alert("Please Add a Task");
+		}
+
+		prop.onAdd({ text, day, remainder });
+		setText("");
+		setDay("");
+		setRemainder(false);
+	};
+
 	return (
-		<>
-			{props.tasks.map(item => (
-				<Task
-					key={item.id}
-					task={item}
-					onDelete={props.onDelete}
-					onToggle={props.onToggle}
+		<form className="add-form" onSubmit={onSubmit}>
+			<div className="form-control">
+				<label>Task</label>
+				<input
+					type="text"
+					placeholder="Add Task"
+					value={text}
+					onChange={taskIs}
 				/>
-			))}
-		</>
+			</div>
+
+			<div className="form-control">
+				<label>Day</label>
+				<input type="text" placeholder="Add Day" value={day} onChange={dayIs} />
+			</div>
+
+			<div className="form-control form-control-check">
+				<label>Set Remainder</label>
+				<input
+					type="checkbox"
+					checked={remainder}
+					value={remainder}
+					onChange={remainderIs}
+				/>
+			</div>
+
+			<input type="submit" value="Save" className="btn btn-block" />
+		</form>
 	);
 };
 
-export default Tasks;
+export default AddTask;
